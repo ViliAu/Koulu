70478039baa4130c6daf8d813d543c081ec2ccbe
Kakkatykki <36374215+ViliAu@users.noreply.github.com>
2021-10-05 21:56:41 +0300

    Update to web applications week 4

--- web-appli-week4/public/js/index.js
+++ web-appli-week4/public/js/index.js
@@ -8,8 +8,33 @@ if(document.readyState !== "loading"){
     })
 }
 
+let ingredientList = [];
+let instructionList = [];
+
 async function initializeCode() {
     document.getElementById("header").innerHTML = await getFood();
+    // Assign ingredient submit button function
+    document.getElementById("add-ingredient").addEventListener("click", () => {
+        const textArea = document.getElementById("ingredients-text");
+        ingredientList.push(textArea.value);
+        console.log(textArea.value);
+        textArea.value = "";
+    });
+    // Assign instruction submit button function
+    document.getElementById("add-instruction").addEventListener("click", () => {
+        const textArea = document.getElementById("instructions-text");
+        instructionList.push(textArea.value);
+        console.log(textArea.value);
+        textArea.value = "";
+    });
+    // Assign submit button function
+    document.getElementById("submit").addEventListener("click", async () => {
+        const name = document.getElementById("name-text");
+        if (name.value.length === 0 || ingredientList.length === 0 || instructionList.length === 0)
+            return;
+        await submitData();
+        name.value = "";
+    });
 }
 
 async function getFood() {
@@ -17,3 +42,25 @@ async function getFood() {
     const data = await res.json();
     return data.recipe;
 }
+
+async function submitData() {
+    const object = {name: document.getElementById("name-text").value,
+        ingredients: ingredientList,
+        instructions: instructionList
+    }
+    console.log(JSON.stringify(object));
+    const res = await fetch("http://[::1]:8000/recipe",  {
+        method: "post",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify(object)});
+}
+
+/*
+method: "post",
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: '{ "poem": "' + poemInput.value + '" }'
+           })
+
+*/
