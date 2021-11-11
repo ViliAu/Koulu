08d6674966f682fe08b9903485a658f79e730c5f
ViliAu <36374215+ViliAu@users.noreply.github.com>
2021-11-11 16:24:11 +0200

    Week 9 done

---
+++ web-appli-week-9/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+import MyContainer from './components/MyContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <MyContainer />
+    </div>
+  );
+}
+
+export default App;
